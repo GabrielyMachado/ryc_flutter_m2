7b3d1a45a776abaae83923e5a65fc83961a43dd3
GabrielyMachado <gabriely.machado@hotmail.com>
2023-03-14 06:29:15 -0300

    inicio do app perguntas

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(PerguntaApp());
+
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionadada = 0;
+
+  void _responder() {
+    setState(() {
+      _perguntaSelecionadada++;
+    });
+    print(_perguntaSelecionadada);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?'
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Column(
+          children: [
+            Text(perguntas[_perguntaSelecionadada]),
+            ElevatedButton(
+              child: Text('Resposta 1'),
+              onPressed: _responder,
+            ),
+            ElevatedButton(
+              child: Text('Resposta 2'),
+              onPressed: _responder,
+            ),
+            ElevatedButton(
+              child: Text('Resposta 3'),
+              onPressed: _responder,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  @override
+  _PerguntaAppState createState() {
+    return _PerguntaAppState();
+  }
+}
